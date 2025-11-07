afd0aa22ad7af9eab6b9a16b4331105498d119cb
Ahm3d-Shaikh <24100265@lums.edu.pk>
2025-11-07 12:53:44 +0500

    feat: add an endpoint to fetch system prompts

--- app/routes/prompt.py
+++ app/routes/prompt.py
@@ -1,9 +1,30 @@
 from fastapi import HTTPException, APIRouter, status, Depends
 from app.models.prompt import PromptCreate
-from app.services.prompt_service import add_system_prompt_to_db
+from app.services.prompt_service import add_system_prompt_to_db, fetch_system_prompts
 from app.deps.auth_deps import get_current_user
 from app.utils.enums.category import Category
+from bson import json_util
+import json
 router = APIRouter()
+
+
+@router.get("/")
+async def get_system_prompt(current_user = Depends(get_current_user)):
+    try:
+        role = current_user["role"]
+        if role != "admin":
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You don't have access to this feature")
+        
+        prompts = await fetch_system_prompts()
+        result_json = json.loads(json_util.dumps(prompts))
+        return {"message": "Prompts Fetched Successfully", "result": result_json}
+    except HTTPException as http_err:
+        raise http_err
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error while fetching system prompts: {str(e)}"
+        )
 
 
 @router.post("/")
